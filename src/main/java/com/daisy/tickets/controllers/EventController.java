620154d2cc52f7010b00586cedf225377cecf56a
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-07-19 19:30:48 +0700

    feat: implement the create event endpoint

---
+++ src/main/java/com/daisy/tickets/controllers/EventController.java
@@ -0,0 +1,43 @@
+package com.daisy.tickets.controllers;
+
+import com.daisy.tickets.domain.CreateEventRequest;
+import com.daisy.tickets.domain.dtos.CreateEventRequestDTO;
+import com.daisy.tickets.domain.dtos.CreateEventResponseDTO;
+import com.daisy.tickets.domain.entities.Event;
+import com.daisy.tickets.mappers.EventMapper;
+import com.daisy.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDTO> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDTO createEventRequestDTO) {
+
+        CreateEventRequest request = eventMapper.fromDTO(createEventRequestDTO);
+        UUID userId = UUID.fromString(jwt.getSubject());
+
+        Event createdEvent = eventService.createEvent(userId, request);
+        CreateEventResponseDTO createdEventResponseDTO = eventMapper.toDTO(createdEvent);
+        return new ResponseEntity<>(createdEventResponseDTO, HttpStatus.CREATED);
+    }
+
+}
