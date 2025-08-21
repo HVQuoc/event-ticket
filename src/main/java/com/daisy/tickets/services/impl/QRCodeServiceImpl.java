fcb3ba5cb40830703c466a9fc77d4005d1704f50
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-08-21 21:25:42 +0700

    feat: implement qr code generation

--- src/main/java/com/daisy/tickets/services/impl/QRCodeServiceImpl.java
+++ src/main/java/com/daisy/tickets/services/impl/QRCodeServiceImpl.java
@@ -2,17 +2,69 @@ package com.daisy.tickets.services.impl;
 
 
 import com.daisy.tickets.domain.entities.QrCode;
+import com.daisy.tickets.domain.entities.QrCodeStatusEnum;
 import com.daisy.tickets.domain.entities.Ticket;
+import com.daisy.tickets.exceptions.QRCodeGenerationException;
+import com.daisy.tickets.repositories.QRCodeRepository;
 import com.daisy.tickets.services.QRCodeService;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class QRCodeServiceImpl implements QRCodeService {
 
+    private static final int QR_HEIGHT = 300;
+    private static final int QR_WIDTH = 300;
+
+    private final QRCodeWriter qrCodeWriter;
+    private final QRCodeRepository qrCodeRepository;
+
     @Override
     public QrCode generateQRCode(Ticket ticket) {
-        return null;
+        try {
+            UUID uniqueId = UUID.randomUUID();
+            String qrCodeImage = generateQrCodeImage(uniqueId);
+
+            QrCode qrCode = new QrCode();
+            qrCode.setId(uniqueId);
+            qrCode.setTicket(ticket);
+            qrCode.setStatus(QrCodeStatusEnum.ACTIVE);
+            qrCode.setValue(qrCodeImage);
+
+            return qrCodeRepository.saveAndFlush(qrCode);
+
+        } catch(WriterException | IOException e) {
+            throw new QRCodeGenerationException("Failed to generate QR Code", e);
+        }
+    }
+
+    private String generateQrCodeImage(UUID uniqueId) throws IOException, WriterException {
+        BitMatrix bitMatrix = qrCodeWriter.encode(
+                uniqueId.toString(),
+                BarcodeFormat.QR_CODE,
+                QR_WIDTH,
+                QR_HEIGHT
+        );
+
+        BufferedImage qrCodeImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(qrCodeImage, "png", baos);
+            byte[] imageBytes = baos.toByteArray();
+            return Base64.getEncoder().encodeToString(imageBytes);
+        }
     }
 }
