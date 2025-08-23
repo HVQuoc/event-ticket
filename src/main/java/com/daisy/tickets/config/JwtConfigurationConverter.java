46fbf70f1f6f64ff0f0787d43c2821b50d01a731
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-08-23 16:35:22 +0700

    feat: add custom jwt authentication converter

---
+++ src/main/java/com/daisy/tickets/config/JwtConfigurationConverter.java
@@ -0,0 +1,42 @@
+package com.daisy.tickets.config;
+
+import org.apache.catalina.LifecycleState;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtConfigurationConverter implements Converter<Jwt, JwtAuthenticationToken> {
+
+    @Override
+    public JwtAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+
+    private Collection<GrantedAuthority> extractAuthorities(Jwt token) {
+        Map<String, Object> realmAccess = token.getClaim("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles
+                .stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
